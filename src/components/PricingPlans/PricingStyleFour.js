0a58a022e6ba4b77bac5e403eb9c20d0c6a5a749
muneeb360 <muneeb@orbit360.net>
2023-11-07 22:02:28 +0500

    icons added for technologies

--- src/components/PricingPlans/PricingStyleFour.js
+++ src/components/PricingPlans/PricingStyleFour.js
@@ -10,20 +10,22 @@ import Shape1 from "../../images/shape1.png"
 import Shape2 from "../../images/shape2.svg"
 import Shape3 from "../../images/shape3.svg"
 import Shape4 from "../../images/shape4.svg"
-import PartnerImg1 from "../../images/partner-img/partner-1.png"
-import PartnerHoverImg1 from "../../images/partner-img/partner-hover1.png"
-import PartnerImg2 from "../../images/partner-img/partner-2.png"
-import PartnerHoverImg2 from "../../images/partner-img/partner-hover2.png"
-import PartnerImg3 from "../../images/partner-img/partner-3.png"
-import PartnerHoverImg3 from "../../images/partner-img/partner-hover3.png"
-import PartnerImg4 from "../../images/partner-img/partner-10.png"
-import PartnerHoverImg4 from "../../images/partner-img/partner-hover10.png"
-import PartnerImg5 from "../../images/partner-img/partner-5.png"
-import PartnerHoverImg5 from "../../images/partner-img/partner-hover5.png"
-import PartnerImg6 from "../../images/partner-img/partner-6.png"
-import PartnerHoverImg6 from "../../images/partner-img/partner-hover6.png"
-import PartnerImg7 from "../../images/partner-img/partner-7.png"
-import PartnerHoverImg7 from "../../images/partner-img/partner-hover7.png"
+import PartnerImg1 from "../../images/partner-img/icons8-node-js-48.png"
+import PartnerHoverImg1 from "../../images/partner-img/icons8-node-js-48.png"
+import PartnerImg2 from "../../images/partner-img/icons8-.net-framework-48.png"
+import PartnerHoverImg2 from "../../images/partner-img/icons8-.net-framework-48.png"
+import PartnerImg3 from "../../images/partner-img/icons8-react-native-48.png"
+import PartnerHoverImg3 from "../../images/partner-img/icons8-react-native-48.png"
+import PartnerImg4 from "../../images/partner-img/icons8-ruby-48.png"
+import PartnerHoverImg4 from "../../images/partner-img/icons8-ruby-48.png"
+import PartnerImg5 from "../../images/partner-img/django_icon.png"
+import PartnerHoverImg5 from "../../images/partner-img/django_icon.png"
+import PartnerImg6 from "../../images/partner-img/icons8-angular-48.png"
+import PartnerHoverImg6 from "../../images/partner-img/icons8-angular-48.png"
+import PartnerImg7 from "../../images/partner-img/icons8-vue-js-48.png"
+import PartnerHoverImg7 from "../../images/partner-img/icons8-vue-js-48.png"
+import mobileappimage1 from "../../images/partner-img/swift_mobile_app.png"
+import flutterimage2 from "../../images/partner-img/flutter_icon.png"
 
 class PricingStyleFour extends Component {
   openTabSection = (evt, tabNmae) => {
@@ -88,66 +90,66 @@ class PricingStyleFour extends Component {
 
               <div className="tab_content">
                 <div id="tab1" className="tabs_item">
-                <div className="ml-partner-area mt-minus-top ptb-80 pb-0">
-                  <div className="container">
-                    <Swiper
-                      slidesPerView={1}
-                      spaceBetween={10}
-                      breakpoints={{
-                        0: {
-                          slidesPerView: 2,
-                        },
-                        768: {
-                          slidesPerView: 4,
-                        },
-                        1200: {
-                          slidesPerView: 6,
-                        },
-                      }}
-                      autoplay={{
-                        delay: 6500,
-                        disableOnInteraction: true,
-                        pauseOnMouseEnter: true,
-                      }}
-                      modules={[Autoplay]}
-                      className="partner-slides"
-                    >
-                      <SwiperSlide className="single-ml-partner">
-                        <img src={PartnerImg1} alt="Partner" />
-                        <img src={PartnerHoverImg1} alt="Partner" />
-                      </SwiperSlide>
+                     <div className="ml-partner-area mt-minus-top ptb-80 pb-0">
+                        <div className="container">
+                          <Swiper
+                            slidesPerView={1}
+                            spaceBetween={10}
+                            breakpoints={{
+                              0: {
+                                slidesPerView: 2,
+                              },
+                              768: {
+                                slidesPerView: 4,
+                              },
+                              1200: {
+                                slidesPerView: 6,
+                              },
+                            }}
+                            autoplay={{
+                              delay: 6500,
+                              disableOnInteraction: true,
+                              pauseOnMouseEnter: true,
+                            }}
+                            modules={[Autoplay]}
+                            className="partner-slides"
+                          >
+                            <SwiperSlide className="single-ml-partner">
+                              <img src={PartnerImg1} alt="Partner" />
+                              <img src={PartnerHoverImg1} alt="Partner" />
+                            </SwiperSlide>
 
-                      <SwiperSlide className="single-ml-partner">
-                        <img src={PartnerImg2} alt="Partner" />
-                        <img src={PartnerHoverImg2} alt="Partner" />
-                      </SwiperSlide>
+                            <SwiperSlide className="single-ml-partner">
+                              <img src={PartnerImg2} alt="Partner" />
+                              <img src={PartnerHoverImg2} alt="Partner" />
+                            </SwiperSlide>
 
-                      <SwiperSlide className="single-ml-partner">
-                        <img src={PartnerImg3} alt="Partner" />
-                        <img src={PartnerHoverImg3} alt="Partner" />
-                      </SwiperSlide>
+                            <SwiperSlide className="single-ml-partner">
+                              <img src={PartnerImg3} alt="Partner" />
+                              <img src={PartnerHoverImg3} alt="Partner" />
+                            </SwiperSlide>
 
-                      <SwiperSlide className="single-ml-partner">
-                        <img src={PartnerImg4} alt="Partner" />
-                        <img src={PartnerHoverImg4} alt="Partner" />
-                      </SwiperSlide>
+                            <SwiperSlide className="single-ml-partner">
+                              <img src={PartnerImg4} alt="Partner" />
+                              <img src={PartnerHoverImg4} alt="Partner" />
+                            </SwiperSlide>
 
-                      <SwiperSlide className="single-ml-partner">
-                        <img src={PartnerImg5} alt="Partner" />
-                        <img src={PartnerHoverImg5} alt="Partner" />
-                      </SwiperSlide>
+                            <SwiperSlide className="single-ml-partner">
+                              <img src={PartnerImg5} alt="Partner" />
+                              <img src={PartnerHoverImg5} alt="Partner" />
+                            </SwiperSlide>
 
-                      <SwiperSlide className="single-ml-partner">
-                        <img src={PartnerImg6} alt="Partner" />
-                        <img src={PartnerHoverImg6} alt="Partner" />
-                      </SwiperSlide>
+                            <SwiperSlide className="single-ml-partner">
+                              <img src={PartnerImg6} alt="Partner" />
+                              <img src={PartnerHoverImg6} alt="Partner" />
+                            </SwiperSlide>
 
-                      <SwiperSlide className="single-ml-partner">
-                        <img src={PartnerImg7} alt="Partner" />
-                        <img src={PartnerHoverImg7} alt="Partner" />
-                      </SwiperSlide>
-                    </Swiper>
-                  </div>
+                            <SwiperSlide className="single-ml-partner">
+                              <img src={PartnerImg7} alt="Partner" />
+                              <img src={PartnerHoverImg7} alt="Partner" />
+                            </SwiperSlide>
+                          </Swiper>
+                        </div>
                     </div>
                 </div>
 
@@ -177,19 +179,19 @@ class PricingStyleFour extends Component {
                         className="partner-slides"
                       >
                         <SwiperSlide className="single-ml-partner">
-                          <img src={PartnerImg1} alt="Partner" />
-                          <img src={PartnerHoverImg1} alt="Partner" />
+                          <img src={mobileappimage1} alt="Partner" />
+                          <img src={mobileappimage1} alt="Partner" />
                         </SwiperSlide>
 
                       
                         <SwiperSlide className="single-ml-partner">
-                          <img src={PartnerImg6} alt="Partner" />
-                          <img src={PartnerHoverImg6} alt="Partner" />
+                          <img src={PartnerHoverImg3} alt="Partner" />
+                          <img src={PartnerHoverImg3} alt="Partner" />
                         </SwiperSlide>
 
                         <SwiperSlide className="single-ml-partner">
-                          <img src={PartnerImg7} alt="Partner" />
-                          <img src={PartnerHoverImg7} alt="Partner" />
+                          <img src={flutterimage2} alt="Partner" />
+                          <img src={flutterimage2} alt="Partner" />
                         </SwiperSlide>
                       </Swiper>
                     </div>
@@ -251,7 +253,7 @@ class PricingStyleFour extends Component {
                 <div id="tab4" className="tabs_item">
                 <div className="ml-partner-area mt-minus-top ptb-80 pb-0">
         <div className="container">
-          <Swiper
+          {/* <Swiper
             slidesPerView={1}
             spaceBetween={10}
             breakpoints={{
@@ -300,7 +302,7 @@ class PricingStyleFour extends Component {
             </SwiperSlide>
 
           
-          </Swiper>
+          </Swiper> */}
         </div>
       </div>
                 </div>
