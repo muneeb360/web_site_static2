07c727e117a724d92e45c4cb9492f3dcb64e3eb9
muneeb360 <muneeb@orbit360.net>
2024-01-18 20:37:38 +0500

    form on home page

--- src/components/Common/Newsletter.js
+++ src/components/Common/Newsletter.js
@@ -19,7 +19,7 @@ const Newsletter = () => {
             </div>
 
             <div className="col-lg-6 col-md-12">
-              <div className="free-trial-content">
+              {/* <div className="free-trial-content">
                 <h2>Start your free trial</h2>
 
                 <form className="newsletter-form">
@@ -35,7 +35,102 @@ const Newsletter = () => {
                   Test out the Machine Learning features for 14 days, no credit
                   card required.
                 </p>
-              </div>
+              </div> */}
+               <form id="contactFormcontact"  className="contactform">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-lg-6 col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12 col-md-12">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        name="number"
+                        placeholder="Phone number"
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12 col-md-12">
+                    <div className="form-group">
+                     
+                      <select name="subject" class="form-control">
+                      <option value="" disabled selected>Select your service model</option>
+                      <option value="service2">Service 2</option>
+                      <option value="service3">Service 3</option>
+                        </select>
+                    </div>
+                  </div>
+                  <div className="col-lg-12 col-md-12">
+                    <div className="form-group">
+                     
+                      <select name="subject" class="form-control">
+                      <option value="" disabled selected>How did you hear about Invozone</option>
+                      <option value="service2">Service 2</option>
+                      <option value="service3">Service 3</option>
+                        </select>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12 col-md-12">
+                    <div className="form-group">
+                      <textarea
+                        name="text"
+                        cols="30"
+                        rows="5"
+                        placeholder="Message"
+                        className="form-control"
+                        style={{ resize:"none" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-12 col-md-12">
+                    <div className="form-group">
+                    <input type="checkbox"
+                     id="termsAndConditions"
+                      name="termsAndConditions"
+                      />
+<label for="termsAndConditions">I agree to the Terms and Conditions</label>
+                    <br></br>
+                  
+                    
+                  
+                    <input type="checkbox"
+                     id="termsAndConditions"
+                      name="termsAndConditions"
+                      />
+<label for="termsAndConditions">get NDA</label>
+                      </div>
+                    </div>
+                  
+
+                  <div className="col-lg-12 col-sm-12">
+                    <button type="submit" className="btn btn-primary">
+                      Send Message
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
